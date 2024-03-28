9adaadcc43d2e04dd3a19292902b2adffeb5d77c
alirfz <seyedalirafazi80@gmail.com>
2024-03-28 20:20:30 +0330

    fix 0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
 });
